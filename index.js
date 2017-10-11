165eb086d059f4c67168ffac87f0aec99e04554f
zzingso <ravie90@gmail.com>
2017-10-11 17:37:49 +0900

    mongoDB connect

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var app = express();
 
-//DB setting
+//DB settings
 //환경변수에 저장된 값을 사용하여 mongoDB에 접속
 mongoose.connect(process.env.MONGO_DB, { useMongoClient : true});
 //mongoose의 db object를 가져와서 db변수에 저장
@@ -18,9 +19,49 @@ db.on("error", function(err){
   console.log("DB error : ", err);
 });
 
+//Other settings
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
+//DB schema
+var contactSchema = mongoose.Schema({
+  name:{type:String, required:true, unique:true},
+  email:{type:String},
+  phone:{type:String}
+});
+
+var Contact = mongoose.model("contact", contactSchema);
+
+//Routes
+//Home
+app.get("/", function(request, response){
+  response.redirect("/contacts");
+});
+
+//Contacts - Index
+app.get("/contacts", function(request, response){
+  Contact.find({}, function(err, contacts){
+    if(err) return response.json(err);
+    response.render("contacts/index", {contacts:contacts});
+  });
+});
+
+//Contacts - New
+app.get("/contacts/new", function(request, response){
+  response.render("contacts/new");
+});
+
+//Contacts - Create
+app.post("/contacts", function(request, response){
+  Contact.create(request.body, function(err, contact){
+    if(err) return response.json(err);
+    response.redirect("/contacts");
+  });
+});
+
+//Port setting
 app.listen(3000, function(){
   console.log("server on!");
 });
