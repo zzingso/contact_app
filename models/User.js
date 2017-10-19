344d52318cb32b7ae682bb2de2dde3893c91391b
zzingso <ravie90@gmail.com>
2017-10-19 09:50:48 +0900

    password bcrypt

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 // models/User.js
 
 var mongoose = require("mongoose");
+var bcrypt = require("bcrypt-nodejs");
 
 // schema
 var userSchema = mongoose.Schema({
@@ -48,7 +49,7 @@ userSchema.path("password").validate(function(v){
     if(!user.currentPassword) {
       user.invalidate("currentPassword", "Current Password is required!");
     }
-    if(user.currentPassword && user.currentPassword != user.originalPassword) {
+    if(user.currentPassword && !bcrypt.compareSync(user.currentPassword, user.originalPassword)) {
       user.invalidate("currentPassword", "Current Password is invalid!");
     }
     if(user.newPassword !== user.passwordConfirmation) {
@@ -56,6 +57,23 @@ userSchema.path("password").validate(function(v){
     }
   }
 });
+
+// hash password
+userSchema.pre("save", function(next) {
+  var user = this;
+  if(!user.isModified("password")) {
+    return next();
+  } else {
+    user.password = bcrypt.hashSync(user.password);
+    return next();
+  }
+});
+
+// model methods
+userSchema.methods.authenticate = function(password) {
+  var user = this;
+  return bcrypt.compareSync(password, user.password);
+};
 
 // model and export
 var User = mongoose.model("user", userSchema);
